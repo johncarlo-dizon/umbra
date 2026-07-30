cf0affa6e91566b6ebc5177c9eb11dd5424410ac
johncarlo-dizon <johncarlovdizon@gmail.com>
2026-07-30 18:02:04 +0800

    Add Vercel build script

---
+++ vercel-build.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+set -e
+
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+export PATH="$PATH:$(pwd)/flutter/bin"
+flutter config --enable-web
+flutter pub get
+flutter build web --release \
+  --dart-define=SUPABASE_URL=$SUPABASE_URL \
+  --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
